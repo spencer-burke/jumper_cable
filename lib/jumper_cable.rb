516bab7625f510870254561b2d9c949e0e850544
spencer burke <info.spencer.burke@gmail.com>
2026-05-08 16:06:59 -0700

    Add better files

---
+++ lib/jumper_cable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jumper_cable/version"
+
+module JumperCable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jumper_cable_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+RSpec.describe JumperCable do
+  it "has a version number" do
+    expect(JumperCable::VERSION).not_to be nil
+  end
+
+end
