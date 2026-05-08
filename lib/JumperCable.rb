970d543672fd2e6c6f43fcf9cd50cbe36aee0d78
spencer burke <info.spencer.burke@gmail.com>
2026-05-08 15:50:54 -0700

    Add gem spec files for incomplete gem

---
+++ lib/JumperCable.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "JumperCable/version"
+
+module JumperCable
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/JumperCable/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module JumperCable
+  VERSION = "0.1.0"
+end
